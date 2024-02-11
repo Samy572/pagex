7bfe155671025a61bbbc3f361753a157ade7f54d
Sam <samy_ch@outlook.fr>
2024-02-11 01:09:55 +0100

    🚧 Start new section price

---
+++ components/price/price.tsx
@@ -0,0 +1,10 @@
+import { Container } from "../layout/container";
+
+const Price = () => {
+	return <section id="price" className="h-[100vh] w-full">
+<Container>
+  <h2>Prix</h2>
+</Container>
+  </section>;
+};
+export default Price;
