6dba44daa3e1fd630007320e29eb66a689e65bb6
Sam <samy_ch@outlook.fr>
2024-02-12 22:52:17 +0100

    💫 Add animation scrollY when card appear and spotlight

--- components/price/cards.tsx
+++ components/price/cards.tsx
@@ -1,4 +1,5 @@
-import { CheckIcon } from '@radix-ui/react-icons';
+'use client';
+import { motion, useInView, useScroll } from 'framer-motion';
 
 import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
@@ -13,62 +14,71 @@ import {
 } from '@/components/ui/card';
 import { getPrice } from '@/lib/getprice';
 import { CheckCircle2 } from 'lucide-react';
+import { useRef } from 'react';
 
 const prices: CardPrice = getPrice;
 
 type Cards = React.ComponentProps<typeof Card>;
 
 export function CardPrices({ className, ...props }: Cards) {
+	const targetRef = useRef<HTMLDivElement | null>(null);
 	return (
 		<>
 			<div className="h-full w-full  flex md:justify-around gap-y-4  md:flex-row flex-col ">
 				{prices.map((price, index) => (
-					<Card
-						style={{ border: `solid 1px ${price.color}` }}
+					<motion.div
 						key={index}
-						className={cn(
-							`md:w-[380px] h-[450px] hover:bg-secondary/40 grid  `,
-							className
-						)}
-						{...props}
+						ref={targetRef}
+						initial={{ opacity: 0, y: 50 }}
+						whileInView={{ opacity: 1, y: 0 }}
+						transition={{ delay: index * 0.4 }}
 					>
-						<CardHeader>
-							<CardTitle
-								className="text-3xl font-bold text-center mb-2"
-								style={{ color: price.color }}
-							>
-								{price.title}
-							</CardTitle>
-							<CardDescription className="text-2xl font-semibold">
-								{price.price}
-							</CardDescription>
-							<CardDescription className="text-xl">
-								{price.optimal}
-							</CardDescription>
-							<CardDescription className="underline text-lg">
-								{price.plus}
-							</CardDescription>
-						</CardHeader>
-						<CardContent className="flex flex-col   gap-4">
-							{price.list.map((el, index) => {
-								return (
-									<div className="w-full flex gap-x-2 " key={index}>
-										<CheckCircle2 color="rgb(59 130 246)" />
-										<span>{el}</span>
-									</div>
-								);
-							})}
-						</CardContent>
-						<CardFooter>
-							<Button
-								variant={'outline'}
-								style={{ backgroundColor: price.color }}
-								className={`w-full  `}
-							>
-								{price.start}
-							</Button>
-						</CardFooter>
-					</Card>
+						<Card
+							style={{ border: `solid 1px ${price.color}` }}
+							className={cn(
+								`md:w-[380px] h-[450px] hover:bg-secondary/40 grid  `,
+								className
+							)}
+							{...props}
+						>
+							<CardHeader>
+								<CardTitle
+									className="text-3xl font-bold text-center mb-2"
+									style={{ color: price.color }}
+								>
+									{price.title}
+								</CardTitle>
+								<CardDescription className="text-2xl font-semibold">
+									{price.price}
+								</CardDescription>
+								<CardDescription className="text-xl">
+									{price.optimal}
+								</CardDescription>
+								<CardDescription className="underline text-lg">
+									{price.plus}
+								</CardDescription>
+							</CardHeader>
+							<CardContent className="flex flex-col   gap-4">
+								{price.list.map((el, index) => {
+									return (
+										<div className="w-full flex gap-x-2 " key={index}>
+											<CheckCircle2 color="rgb(59 130 246)" />
+											<span>{el}</span>
+										</div>
+									);
+								})}
+							</CardContent>
+							<CardFooter>
+								<Button
+									variant={'outline'}
+									style={{ backgroundColor: price.color }}
+									className={`w-full  `}
+								>
+									{price.start}
+								</Button>
+							</CardFooter>
+						</Card>
+					</motion.div>
 				))}
 			</div>
 		</>
--- components/price/price.tsx
+++ components/price/price.tsx
@@ -1,14 +1,24 @@
+'use client';
+import { useRef } from 'react';
 import { CardPrices } from './cards';
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 const Price = () => {
 	return (
 		<section
 			id="price"
-			className="h-[100vh]  w-full flex items-center flex-col"
+			className="h-full  w-full flex items-center flex-col relative"
 		>
-			<h3 className="font-semibold my-12 text-3xl ">
+			<div className="absolute z-20 top-40 left-0 h-64 w-60 violet-gradient blur-[150px]"></div>
+			<div className=" absolute z-20  top-96 right-0 h-64 w-40 cyan-gradient blur-[150px]"></div>
+			<motion.h3
+				initial={{ opacity: 0, y: 150 }}
+				whileInView={{ opacity: 1, y: 0 }}
+				className="font-semibold my-36 lg:text-6xl text-4xl "
+				transition={{ duration: 0.3 }}
+			>
 				Des offres adaptés à vos besoins
-			</h3>
+			</motion.h3>
 
 			<div className="w-full flex  max-w-7xl mx-auto items-center ">
 				<CardPrices />
