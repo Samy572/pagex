de01480d0d1e65698e977b1e77bafb11bc0a47ab
Sam <samy_ch@outlook.fr>
2024-02-13 17:52:28 +0100

    Create file question/answer

---
+++ lib/getfaq.ts
@@ -0,0 +1,19 @@
+export const getFaq = [
+	{
+		question: `Y-a-t-il des coûts cachés associés à ses offres ?`,
+		answer: `Non, il n'y a aucun coût caché associé à nos offres. Les tarifs que nous affichons comprennent toutes les fonctionnalités et services mentionnés`,
+	},
+  {
+    question: `Y-a-t-il des fonctionnalités personnalisées à des besoins spécifiques ?`,
+    answer: `Oui, nous avons des fonctionnalités personnalisées et addaptées à vos besoins. Vous pouvez nous contacter pour plus de renseignement`,
+  }, 
+  {
+    question: 'Les tarifs indiqués sont-ils mensuels ou annuels ?',
+    answer: 'Tous les tarifs affichés correspondent à une commande specifique.',
+  }, 
+
+  {
+    question: `Comment puis-je contacter votre équipe pour discuter de l'offre Premium ?`,
+    answer: `Vous trouverez nos coordonnées de contact dans la section Contact`,
+  }
+];
