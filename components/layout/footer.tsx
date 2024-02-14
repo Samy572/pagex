3f94d686326d2d2eee437a3ea76adedcdc923d8b
Sam <samy_ch@outlook.fr>
2024-02-14 22:42:25 +0100

    ♻️ Refacto footer and add Animation

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,88 +1,134 @@
+'use client';
 import clsx from 'clsx';
 import { orbitron } from './font';
+import { motion } from 'framer-motion';
 import Image from 'next/image';
 import Link from 'next/link';
+import {
+	GetFooterAccueil,
+	GetProduitFooter,
+	GetSocialMedia,
+} from '@/lib/getlink';
 const Footer = () => {
+	const linkHomeFooter = GetFooterAccueil;
+	const linkProductFooter = GetProduitFooter;
+	const linkSocialMedia = GetSocialMedia;
+	const variants = {
+		hidden: { opacity: 0, y: 50 },
+		visible: { opacity: 1, y: 0 },
+	};
+
 	return (
-		<footer className=" w-full bg-blue-500">
+		<motion.footer
+			variants={variants}
+			initial={variants.hidden}
+			whileInView={variants.visible}
+			transition={{ duration: 0.5, delay: 0.1 }}
+			className=" w-full bg-blue-500"
+		>
 			<div className="">
 				<div className=" max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8 grid md:grid-cols-3 grid-cols-1  py-7 border-b-2 border-white">
 					<div className="lg:w-1/2 flex items-center">
-						<h4
+						<motion.h4
+							initial={{ y: -50, opacity: 0 }}
+							whileInView={{ opacity: 1, y: 0 }}
+							transition={{ duration: 0.5, delay: 0.5 }}
 							className={clsx(orbitron.className, 'md:text-5xl text-3xl pb-5')}
 						>
 							Page X
-						</h4>
+						</motion.h4>
 					</div>
 					<ul className="leading-8">
-						<h5 className="text-xl pb-4 underline">Accueil</h5>
-						<li>
-							<Link href={'/'}>Accueil</Link>
-						</li>
-						<li>
-							<Link href={'#price'}> Prix</Link>
-						</li>
-						<li>
-							<Link href={'#faq'}>Faq</Link>
-						</li>
-						<li>
-							<Link href={'#contact'}>Contact</Link>
-						</li>
+						<motion.h5
+							variants={variants}
+							initial={variants.hidden}
+							whileInView={variants.visible}
+							transition={{ duration: 0.5, delay: 0.4 }}
+							className="text-xl pb-4 underline"
+						>
+							Accueil
+						</motion.h5>
+						{linkHomeFooter.map((el) => {
+							return (
+								<motion.li
+									initial={variants.hidden}
+									variants={variants}
+									whileInView={variants.visible}
+									transition={{ duration: 0.5, delay: 0.5 + el.id * 0.1 }}
+									key={el.id}
+								>
+									<Link href={el.name}>{el.name}</Link>
+								</motion.li>
+							);
+						})}
 					</ul>
 					<ul className="leading-8">
-						<h5 className="text-xl pb-4 underline">Produits</h5>
-						<li>
-							{' '}
-							<Link href={'#price'}> Essentiel</Link>
-						</li>
-						<li>
-							{' '}
-							<Link href={'#price'}> Pro</Link>
-						</li>
-						<li>
-							{' '}
-							<Link href={'#price'}> Premium</Link>
-						</li>
+						<motion.h5
+							variants={variants}
+							initial={variants.hidden}
+							whileInView={variants.visible}
+							transition={{ duration: 0.5, delay: 0.4 }}
+							className="text-xl pb-4 underline"
+						>
+							Produits
+						</motion.h5>
+						{linkProductFooter.map((el) => {
+							return (
+								<motion.li
+									key={el.id}
+									variants={variants}
+									initial={variants.hidden}
+									whileInView={variants.visible}
+									transition={{ duration: 0.5, delay: 0.5 + el.id * 0.1 }}
+								>
+									<Link href={el.name}>{el.name}</Link>
+								</motion.li>
+							);
+						})}
 					</ul>
 				</div>
 				<div className=" max-w-7xl w-full h-full mx-auto px-4 sm:px-6 lg:px-8  py-7 flex flex-col">
-					<h5 className="text-xl pb-4 underline">Nos réseaux</h5>
+					<motion.h5
+						variants={variants}
+						initial={variants.hidden}
+						whileInView={variants.visible}
+						transition={{ duration: 0.5, delay: 0.4 }}
+						className="text-xl pb-4 underline"
+					>
+						Nos réseaux
+					</motion.h5>
 					<div className="flex md:justify-between flex-col md:flex-row ">
 						<ul className="leading-8 flex gap-x-7 ">
-							<li>
-								<Image
-									src="/images/svg/twitter.svg"
-									alt="logo"
-									width={20}
-									height={20}
-								/>
-							</li>
-							<li>
-								{' '}
-								<Image
-									src="/images/svg/fb.svg"
-									alt="logo"
-									width={20}
-									height={20}
-								/>
-							</li>
-							<li>
-								<Image
-									src="/images/svg/insta.svg"
-									alt="logo"
-									width={20}
-									height={20}
-								/>{' '}
-							</li>
+							{linkSocialMedia.map((el) => {
+								return (
+									<motion.li
+										key={el.id}
+										initial={variants.hidden}
+										variants={variants}
+										whileInView={variants.visible}
+										transition={{ duration: 0.5, delay: 0.5 + el.id * 0.1 }}
+									>
+										<Link target="_blank" href={el.href}>
+											<Image src={el.img} alt="logo" width={20} height={20} />
+										</Link>
+									</motion.li>
+								);
+							})}
 						</ul>
-						<p className="text-lg mt-5 md:m-0">
+						<motion.p
+							variants={variants}
+							initial={variants.hidden}
+							whileInView={variants.visible}
+							transition={{ duration: 0.5, delay: 0.4 }}
+							className="text-lg mt-5 md:m-0"
+						>
 							© 2024 <span className={orbitron.className}>Page X</span>. Tous
 							les droits sont réservés.
-						</p>
+						</motion.p>
 					</div>
 				</div>
 			</div>
-		</footer>
+		</motion.footer>
 	);
 };
 export default Footer;
--- lib/getlink.ts
+++ lib/getlink.ts
@@ -20,3 +20,65 @@ export const GetLink = [
 		href: '#contact',
 	},
 ];
+
+export const GetFooterAccueil = [
+	{
+		id: 1,
+		name: 'Accueil',
+		href: '/',
+	},
+	{
+		id: 2,
+		name: 'Prix',
+		href: '#price',
+	},
+	{
+		id: 3,
+		name: 'Faq',
+		href: '#faq',
+	},
+	{
+		id: 4,
+		name: 'Contact',
+		href: '#contact',
+	},
+];
+
+export const GetProduitFooter = [
+	{
+		id: 1,
+		name: 'Essentiel',
+		href: '#price',
+	},
+	{
+		id: 2,
+		name: 'Pro',
+		href: '#price',
+	},
+	{
+		id: 3,
+		name: 'Premium',
+		href: '#price',
+	},
+];
+
+export const GetSocialMedia = [
+	{
+		id: 1,
+		name: 'Facebook',
+		href: 'https://facebook.com/',
+		img: 'images/svg/fb.svg',
+	},
+	{
+		id: 2,
+		name: 'Twitter',
+		href: 'https://twitter.com/',
+		img: 'images/svg/twitter.svg',
+	},
+	{
+		id: 3,
+		name: 'Instagram',
+		href: 'https://instagram.com/',
+		img: 'images/svg/insta.svg',
+	},
+];
