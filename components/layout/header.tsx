e29039cd22e1d85ca91f827f2ca7d32005b105a1
Sam <samy_ch@outlook.fr>
2024-03-17 21:29:18 +0100

    ✨ Add cool hover navbar transition

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -4,14 +4,18 @@ import { ModeToggle } from '../ui/toggle-mode';
 import { orbitron } from './font';
 import { motion } from 'framer-motion';
 import { GetLink } from '@/lib/getlink';
-import { useState } from 'react';
+import { useRef } from 'react';
 import { Menu } from './menu';
 import { TypeLink } from '@/types/types';
 const Header = () => {
-	const [activeLinkId, setActiveLinkId] = useState<number | null>(null);
-
-	const handleClick = (linkId: number) => {
-		setActiveLinkId(linkId === activeLinkId ? null : linkId);
+	const hoverBar = useRef<HTMLDivElement>(null);
+	const handleHover = (e: React.MouseEvent<HTMLLIElement>) => {
+		const target = e.currentTarget;
+		if (hoverBar.current) {
+			hoverBar.current.style.transform = `translateX(${target.offsetLeft}px)`;
+			hoverBar.current.style.width = `${target.scrollWidth}px`;
+			hoverBar.current.style.height = `${target.offsetHeight}px`;
+		}
 	};
 	const getlink: TypeLink = GetLink;
 
@@ -24,19 +28,18 @@ const Header = () => {
 				initial={{ opacity: 0, y: 30 }}
 				animate={{ opacity: 1, y: 0 }}
 				transition={{ duration: 0.5, delay: 0.2 }}
-				className="md:flex items-center hidden gap-x-3 "
+				className="md:flex items-center hidden gap-x-3 relative"
 			>
 				{getlink.map((link) => (
 					<li
 						key={link.name}
-						onClick={() => handleClick(link.id)}
-						className={`${
-							activeLinkId === link.id ? 'activelink ' : ''
-						}  transition-all duration-300 ease-in-out p-1 hover:bg-secondary dark:hover:text-white rounded-md`}
+						onMouseOver={(e) => handleHover(e)}
+						className=" py-1.5 px-2.5   rounded-md"
 					>
 						<Link href={link.href}>{link.name}</Link>
 					</li>
 				))}
+				<div ref={hoverBar} className="hover-bar"></div>
 				<li>
 					<ModeToggle />
 				</li>
